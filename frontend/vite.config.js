21a415857a5a601b0bc979f14d5ab7c92e1224f1
Hitonimu <darulahyar4468@gmail.com>
2026-03-08 19:54:12 +0700

    add frontend env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
     proxy: {
-      // Proxy /api requests to FastAPI during development
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://weather-dashboard-production-ddde.up.railway.app',
         changeOrigin: true,
-      },
-    },
-  },
+        secure: true,
+      }
+    }
+  }
 })
